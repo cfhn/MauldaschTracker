3ae3550ae272694134b28cbb691ff77f9ae4ace5
Patrick Krämer <info@patagona.dev>
2025-05-27 01:34:35 +0200

    add shitty authentication

--- MauldaschTracker/Program.cs
+++ MauldaschTracker/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using idunno.Authentication.Basic;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,35 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(BasicAuthenticationDefaults.AuthenticationScheme)
+    .AddBasic(options =>
+    {
+        options.Realm = "Basic Authentication";
+        options.Events = new BasicAuthenticationEvents
+        {
+            OnValidateCredentials = context =>
+            {
+                var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+                var password = config.GetValue<string>("AdminPassword");
+                if (!string.IsNullOrWhiteSpace(password) && password == context.Password)
+                {
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, context.Username, ClaimValueTypes.String, context.Options.ClaimsIssuer),
+                        new Claim(ClaimTypes.Name, context.Username, ClaimValueTypes.String, context.Options.ClaimsIssuer)
+                    };
+
+                    context.Principal = new ClaimsPrincipal(new ClaimsIdentity(claims, context.Scheme.Name));
+                    context.Success();
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+        options.AllowInsecureProtocol = true;
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -15,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 var trackerService = new MauldaschTrackerService(app.Configuration.GetConnectionString("Sql") ?? throw new ArgumentException("Missing SQL connection string"));
 
@@ -35,58 +68,54 @@ app.MapPost("/api/Item/Add", async (AddItemsRequest request) =>
     // Return PDF(?)
     return Results.Ok();
 })
-.WithName("AddItem");
+.WithName("AddItem")
+.RequireAuthorization();
 
 app.MapGet("/api/Item/GetList", async () =>
 {
     return await trackerService.GetItems();
 })
-.WithName("GetItems");
+.WithName("GetItems")
+.RequireAuthorization();
 
 app.MapGet("/api/Collection/GetList", async () =>
 {
-    // var id1 = Guid.NewGuid();
-    // var id2 = Guid.NewGuid();
-    // var id3 = Guid.NewGuid();
-    // var id4 = Guid.NewGuid();
-    // return new List<GetCollectionsResultItem>{
-    //     new GetCollectionsResultItem(id2, "Palette 1", id1, 10),
-    //     new GetCollectionsResultItem(id3, "Palette 2", id1, 10),
-    //     new GetCollectionsResultItem(id1, "LKW", null, 0),
-    //     new GetCollectionsResultItem(id4, "MSE", null, 0)
-    // };
-
     return await trackerService.GetCollections();
 })
-.WithName("GetCollections");
+.WithName("GetCollections")
+.RequireAuthorization();
 
 app.MapPost("/api/Collection/Add", async (AddCollectionRequest request) =>
 {
     await trackerService.AddCollection(request.Name);
     return Results.Ok();
 })
-.WithName("AddCollection");
+.WithName("AddCollection")
+.RequireAuthorization();
 
 app.MapPost("/api/Collection/Delete", async (DeleteCollectionRequest request) =>
 {
     await trackerService.DeleteCollection(request.Id);
     return Results.Ok();
 })
-.WithName("DeleteCollection");
+.WithName("DeleteCollection")
+.RequireAuthorization();
 
 app.MapPost("/api/SetCollection", async (SetCollectionRequest request) =>
 {
     await trackerService.UpdateLocation(request.Items, request.Collections, request.ParentCollection, null, null, null);
     return Results.Ok();
 })
-.WithName("SetCollection");
+.WithName("SetCollection")
+.RequireAuthorization();
 
 app.MapPost("/api/SetPosition", async (SetPositionRequest request) =>
 {
     await trackerService.UpdateLocation(request.Items, request.Collections, null, request.Latitude, request.Longitude, request.Accuracy);
     return Results.Ok();
 })
-.WithName("SetPosition");
+.WithName("SetPosition")
+.RequireAuthorization();
 
 app.UseDefaultFiles();
 app.UseStaticFiles(new StaticFileOptions()
